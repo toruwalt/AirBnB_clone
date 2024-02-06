2760a84019e41f2cf28a1985aa11b22a39fc3388
Torubiri Walter <toruwalt1997@gmail.com>
2024-02-06 08:48:37 +0100

    Made attributes to be private, added getters for attributes id, created_at and updated_at, next task would be the __str__

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3
+
 """This is the base model of the entire AirBnB clone web app"""
 import uuid
 import datetime
 
-class BaseModel:
-    """This class  defines all common attributes and methods for the other classes"""
 
-    def __init__(self, name = None, my_number = None):
+class BaseModel:
+
+    """This class  defines all common attributes \n
+    and methods for the other classes"""
+
+    def __init__(self, name=None, my_number=None):
         """This runs anytime an object is instantiated"""
-        BaseModel.__id = str(uuid.uuid4())
+        self.__id = str(uuid.uuid4())
         self.__created_at = datetime.datetime.now()
         self.__updated_at = datetime.datetime.now()
-        self.__name = name
+        if type(name) != str:
+            raise TypeError("name must be a string")
+        else:
+            self.__name = name
         if type(my_number) != int:
-            raise TypeError("number must be an integer")
+            raise TypeError("name must be a string")
         elif my_number <= 0:
-            raise ValueError("number must be >= 0")
+            raise ValueError("number must be greater than 0")
         else:
             self.__my_number = my_number
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def created_at(self):
+        return self.__created_at
+
+    @property
+    def updated_at(self):
+        return self.__id
 
     @property
     def name(self):
@@ -27,4 +46,22 @@ class BaseModel:
     @name.setter
     def name(self, input):
         """This is the setter of the name porperty"""
-        self.__name = input
+        if type(input) != str:
+            raise TypeError("name must be a string")
+        else:
+            self.__name = input
+
+    @property
+    def my_number(self):
+        """This is the getter of the name property"""
+        return self.__my_number
+
+    @my_number.setter
+    def my_number(self, value_number):
+        """This is the setter of the name porperty"""
+        if type(value_number) != int:
+            raise TypeError("name must be a string")
+        elif value_number <= 0:
+            raise ValueError("number must be greater than 0")
+        else:
+            self.__my_number = value_number
--- test_script.py
+++ test_script.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
-User = BaseModel()
+User = BaseModel("Adam", 89)
+print(User)
 print(User.id)
+print(User.name)
+
+User.name = "My First Name"
+User.my_number = 89
+print(User.id)
+print(User.name)
+print(User.my_number)
 print(User.created_at)
 print(User.updated_at)
