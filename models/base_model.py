f804d92786716d1ca2b929c91db641ed9b3b22df
Torubiri Walter <toruwalt1997@gmail.com>
2024-02-06 07:06:06 +0100

    Added Error handling to catch instances of invalid numbers

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,16 @@ import datetime
 class BaseModel:
     """This class  defines all common attributes and methods for the other classes"""
 
-    def __init__(self):
-        BaseModel.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, name = None, my_number = None):
+        """This runs anytime an object is instantiated"""
+        BaseModel.__id = str(uuid.uuid4())
+        self.__created_at = datetime.datetime.now()
+        self.__updated_at = datetime.datetime.now()
+        self.__name = name
+        if type(my_number) != int:
+            raise TypeError("number must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__my_number = my_number
+        self.__name = name
