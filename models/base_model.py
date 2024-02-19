bbd60a77f4a2084545e0fb296d0a87244e7f92e9
Torubiri Walter <toruwalt1997@gmail.com>
2024-02-19 01:53:24 +0100

    Task 1 totally completed

--- models/base_model.py
+++ models/base_model.py
@@ -13,24 +13,23 @@ class BaseModel:
 
     def __init__(self, name="", my_number=0):
         """This runs anytime an object is instantiated"""
-        self.__id = str(uuid.uuid4())
-        self.__created_at = dateTime.now()
-        self.__updated_at = dateTime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = dateTime.now()
+        self.updated_at = dateTime.now()
 
     def __str__(self):
         """This is the string representation of the object"""
         """[<class name>] (<self.id>) <self.__dict__>"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.__id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        self.__created_at = dateTime.now()
+        self.updated_at = dateTime.now()
 
     def to_dict(self):
-        self.__created_at = self.__created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.__updated_at = self.__updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-
         new__dict = {}
         for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                value = value.isoformat()
             new__dict[key] = value
             new__dict["__class__"] = self.__class__.__name__
         return new__dict
