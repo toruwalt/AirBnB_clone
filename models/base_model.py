ec9c92d0656af748676204198c405e7b616dcf83
Torubiri Walter <toruwalt1997@gmail.com>
2024-02-19 02:20:19 +0100

    Task 2 works

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,23 @@ class BaseModel:
     """This class  defines all common attributes \n
     and methods for the other classes"""
 
-    def __init__(self, name="", my_number=0):
+    def __init__(self, *args, **kwargs):
         """This runs anytime an object is instantiated"""
-        self.id = str(uuid.uuid4())
-        self.created_at = dateTime.now()
-        self.updated_at = dateTime.now()
+
+        if kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.__dict__["created_at"] = dateTime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = dateTime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = dateTime.now()
+            self.updated_at = dateTime.now()
 
     def __str__(self):
         """This is the string representation of the object"""
