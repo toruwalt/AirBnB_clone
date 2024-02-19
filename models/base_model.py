5bc84ad3cc9f2aedacd9c2b9b038251fabce8728
Torubiri Walter <toruwalt1997@gmail.com>
2024-02-19 01:19:49 +0100

    Removed all exceptions, that will be taken care of in the testfiles

--- models/base_model.py
+++ models/base_model.py
@@ -13,18 +13,6 @@ class BaseModel:
 
     def __init__(self, name="", my_number=0):
         """This runs anytime an object is instantiated"""
-        if ((my_number) and (type(my_number) != int)):
-            raise TypeError("number must be an integer")
-        elif ((my_number) and (my_number < 0)):
-            raise ValueError("number must be greater than 0")
-        else:
-            self.__my_number = my_number
-
-        if ((name) and type(name) != str):
-            raise TypeError("name must be a string")
-        else:
-            self.__name = name
-
         self.__id = str(uuid.uuid4())
         self.__created_at = dateTime.now()
         self.__updated_at = dateTime.now()
