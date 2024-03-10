d42ef4fcd0bcc4db551c2d03afab1246f3647553
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-10 02:17:05 +0000

    Imported State Class into City Class correctly

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Model of the City that inherits from BaseModel"""
 from models.base_model import BaseModel
-from models.base_model import State
+from models.state import State
 
 
 class City(BaseModel):
